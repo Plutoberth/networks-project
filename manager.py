547da6c7c0c58e22543fe255b13a682175ad477a
Nir <nir@nirharel.space>
2018-06-09 14:20:33 +0300

    add data placer and html report creator

--- manager.py
+++ manager.py
@@ -1,15 +1,21 @@
 import socket
+import json
+import datetime
 
 SOCKET_DETAILS = ("", 28972)
 
 SETTINGS_PATH = "settings.dat"
+JSON_PATH = "datastore.json"
+
+HTML_ASSOC_DICT = {'ext_port': "PORTS", 'ext_ip': "IPS", 'country': "COUNTRIES", 'traffic_incoming': "AGENTS_IN",
+                   'traffic_outgoing': 'AGENTS_OUT'}
 
 
-def read_dat_file(dat_path: str) -> (dict, dict):
+def read_dat_file(dat_path: str) -> dict:
     """
     The function reads the data file (only compliant files) and parses it.
     :param dat_path: The path to the data file.
-    :return: a dictionary of the workers and a dictionary of the blacklist in a tuple.
+    :return: a dictionary of the workers  of the blacklist.
     """
     try:
         with open(dat_path, "r") as f:
@@ -37,7 +43,7 @@ def read_dat_file(dat_path: str) -> (dict, dict):
         blacklist_split = entry.split(":")
         blacklist_dict[blacklist_split[0]] = blacklist_split[1]
 
-    return workers_dict, blacklist_dict
+    return {"workers": workers_dict, "blacklist": blacklist_dict}
 
 
 def get_reports() -> (bytes, str):
@@ -49,18 +55,102 @@ def get_reports() -> (bytes, str):
     sock.bind(SOCKET_DETAILS)
 
     while True:
-        yield sock.recvfrom(128000)
+        yield sock.recvfrom(256000)
+
+
+def update_dict(dictionary, key, value):
+    """
+    If the key is in the dictionary, add the value to it. Otherwise, put the key in the dictionary.
+    :return: dicts are passed by reference so we don't need to return anything
+    """
+    if key in dictionary:
+        dictionary[key] += value
+
+    else:
+        dictionary[key] = value
+
+
+def update_json(data_dict, json_path, program_settings) -> None:
+    """
+    Updates the json file with our new data. Creates a new one if it doesn't exist.
+    :param data_dict: A dictionary with the data from the agent.
+    :param json_path:
+    :param program_settings: A dict representing the data file.
+    :return:
+    """
+    try:
+        with open(json_path) as f:
+            current_dict = json.loads(f.readline())
+
+    except FileNotFoundError:
+        current_dict = {'ext_ip': {}, 'country': {}, 'ext_port': {}, 'traffic_incoming': {}, 'traffic_outgoing': {}}
+
+    for packet in data_dict:
+        if packet["pvt_ip"] in program_settings["workers"]:
+            worker = program_settings['workers'][packet['pvt_ip']]
+        else:
+            worker = "Unknown"  # We could have a lot of unidentified agents on the network.
+
+        packet_size = packet['packet_size']
+
+        for field in ['ext_ip', 'country', 'ext_port']:
+            field_data = packet[field]
+            update_dict(current_dict[field], field_data, packet_size)
+
+        if packet["direction"] == "i":  # Incoming traffic
+            update_dict(current_dict['traffic_incoming'], worker, packet_size)
+        elif packet["direction"] == "o":  # Outgoing traffic
+            update_dict(current_dict['traffic_outgoing'], worker, packet_size)
+
+    with open(json_path, "w") as f:  # Overwrite
+        f.write(json.dumps(current_dict))
+
+
+def update_html(json_path: str, template_path: str, final_path: str):
+    """
+    Creates a new html file from the template with the data in the json.
+    :param json_path: The json to get the data from.
+    :param template_path: The template to use.
+    :param final_path: What we'll save the html file as.
+    """
+    try:
+        with open(json_path) as f:
+            data: dict = json.loads(f.readline())
+
+        with open(template_path) as f:
+            template: list = f.readlines()
+    except FileNotFoundError:
+        print("JSON or HTML template not found. Returning...")
+        return
+
+    for field_name, field_dict in data.items():
+        html_name = HTML_ASSOC_DICT[field_name]  # Translate the value to the one mentioned in the html file.
+        key_string = f"%%{html_name}_KEYS%%"
+        value_string = f"%%{html_name}_VALUES%%"
+
+        key_line: int = [r[0] for r in enumerate(template) if key_string in r[1]]  # Find the key line
+        value_line: int = [r[0] for r in enumerate(template) if value_string in r[1]]  # Find the value line
+
+        template[key_line] = template[key_line].replace(key_string, json.dumps(field_dict.keys))  # Replace
+
+        template[value_line] = template[value_line].replace(value_string, json.dumps(field_dict.values)) # Insert values
+
+    timestamp_line: int = [r[0] for r in enumerate(template) if "%%TIMESTAMP%%" in r[1]]  # Find the timestamp line
+    template[timestamp_line] = template[timestamp_line].replace("%%TIMESTAMP", datetime.datetime.now())
+
+    with open(final_path, "w") as f:
+        f.writelines(template)
 
 
 def main():
-    workers, blacklist = read_dat_file(SETTINGS_PATH)
+    program_settings = read_dat_file(SETTINGS_PATH)
 
     for data, addr in get_reports():
+        data_dict = json.loads(data.decode())
+        update_json(data_dict, JSON_PATH, program_settings)
+        1/0
 
-
-
-
-
+        update_html(JSON_PATH, None, None)
 
 
 if __name__ == '__main__':
