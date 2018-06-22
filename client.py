33cdde3303ee494d858114b6c16cb9eb6ad7b378
Nir <nir@nirharel.space>
2018-06-22 20:22:19 +0300

    Add proper netstat matching

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 from scapy3k.all import *
 import requests
 import json
+import subprocess
 import threading
 
 API_ADDR = "http://ip-api.com/json/{}"
@@ -8,6 +9,11 @@ PACKET_AMOUNT = 100
 LAN_DETAILS = {}
 PACKET_DATA = []
 MANAGER_DETAILS = ("127.0.0.1", 28972)
+NETSTAT_REGEX = r"({} .+)([^\[])* \[([a-zA-Z.]+)]"
+PVT_IP_REGEX = [re.compile(r"^127.\d{1,3}.\d{1,3}.\d{1,3}$"),
+                re.compile(r"^10.\d{1,3}.\d{1,3}.\d{1,3}$"),
+                re.compile(r"^192.168.\d{1,3}$"),
+                re.compile(r"^172.(1[6-9]|2[0-9]|3[0-1]).[0-9]{1,3}.[0-9]{1,3}$")]
 
 
 class IpCountry:
@@ -32,20 +38,24 @@ class IpCountry:
         :return: Country string.
         """
 
-        try:
-            ip_data = requests.get(API_ADDR.format(ip))
-            time.sleep(5)
-            print(ip)
-        except requests.exceptions.RequestException as e:  # All requests exceptions inherit from this exception
-            print(e)
-            return "Unknown"
+        regex_results = [pat.match(ip) for pat in PVT_IP_REGEX]
+        if all(not r for r in regex_results):  # If all values were none (i.e. external IP)
+            try:
+                ip_data = requests.get(API_ADDR.format(ip))
+                time.sleep(5)
+                print(ip)
+            except requests.exceptions.RequestException as e:  # All requests exceptions inherit from this exception
+                print(e)
+                return "Unknown"
 
-        data_dict = json.loads(ip_data.content)
-        if data_dict["status"] == "fail":
-            return "Private IP"
+            data_dict = json.loads(ip_data.content)
+            if data_dict["status"] == "fail":
+                return "Private IP"  # Just incase it slipped through the regex.
 
+            else:
+                return data_dict["country"]
         else:
-            return data_dict["country"]
+            return "Private IP"
 
 
 class PacketHandler(threading.Thread):
@@ -72,11 +82,31 @@ def sniff_filter(packet: Packet):
     if IP in packet:
         return UDP in packet or TCP in packet
 
+
+def get_netstat_output() -> str:
+    """Returns the output of the netstat command."""
+    netstat = subprocess.Popen(["netstat", "-nb"], stdout=subprocess.PIPE).stdout  # Use the netstat command
+    netstat = [r.decode() for r in netstat]  # Decode the bytes
+    netstat = "".join(netstat)  # Join the output into a single string.
+    return netstat
+
+
 def get_program(local_port: int) -> str:
     """Returns a string of the program that is using the specified local port using netstat."""
     local_ip = LAN_DETAILS["my_ip"]
     local_conn = f"{local_ip}:{local_port}"  # This is the unique connection we need to find within the netstat output.
-    return "Unknown"
+    netstat_output = get_netstat_output()
+
+    local_conn = local_conn.replace(".", r"\.")  # . is a character that represents any character on regex.
+    conn_regex = NETSTAT_REGEX.format(local_conn)  # Format the connection details into the regex pattern.
+
+    re_match = re.search(conn_regex, netstat_output, re.MULTILINE)  # Search for it
+    if re_match:  # If matched
+        program = re_match.group(3)  # get group number 3, which is the exe.
+    else:
+        program = "Unknown"  # Just return a generic unknown.
+
+    return program
 
 
 def record_details(packet: Packet):
@@ -88,7 +118,7 @@ def record_details(packet: Packet):
     if LAN_DETAILS["my_mac"] == packet[Ether].src:
         direction = "o"
 
-    if direction == "i":
+    if direction == "i":  # Get external details.
         ext_ip = packet[IP].src
         ext_port = packet_transport.sport
         local_port = packet_transport.dport
@@ -102,7 +132,7 @@ def record_details(packet: Packet):
     program = get_program(local_port)
 
     packet_dict = dict(ext_ip=ext_ip, ext_port=ext_port, direction=direction, packet_size=packet_size, country="",
-                       program=program)
+                       program=program)  # Store in dictionary.
 
     PACKET_DATA.append(packet_dict)
 
@@ -129,10 +159,8 @@ def main():
             curr_thread.start()
             threads.append(curr_thread)
 
-
     except KeyboardInterrupt:
         print("Aborted agent operation.")
-
 
 
 if __name__ == '__main__':
