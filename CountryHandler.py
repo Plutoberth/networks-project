1b27191693b6d9e12e3132937464898c58a65b0c
Nir <nir@nirharel.space>
2018-06-24 21:11:43 +0300

    Separate the ip country handler to a different file

---
+++ CountryHandler.py
@@ -0,0 +1,56 @@
+import re
+import requests
+
+
+
+API_ADDR = "http://ip-api.com/json/{}"
+PVT_IP_REGEX = [re.compile(r"^127.\d{1,3}.\d{1,3}.\d{1,3}$"),
+                re.compile(r"^10.\d{1,3}.\d{1,3}.\d{1,3}$"),
+                re.compile(r"^192.168.\d{1,3}$"),
+                re.compile(r"^172.(1[6-9]|2[0-9]|3[0-1]).[0-9]{1,3}.[0-9]{1,3}$")]
+
+class IpCountry:
+    def __init__(self):
+        self.ip_dict = {}  # Store associations
+        self.num = 0
+        self.delay = 1
+
+    def __getitem__(self, ip):
+        """
+        Gets the IP's country and caches it.
+        :param ip: The ip to get the country for.
+        :return: The country
+        """
+        if ip not in self.ip_dict:
+            self.ip_dict[ip] = self.get_country(ip)
+        return self.ip_dict[ip]
+
+    def get_country(self, ip: str) -> str:
+        """
+        Gets the country of an ip.
+        :param ip: String of an ip.
+        :return: Country string.
+        """
+
+        regex_results = [pat.match(ip) for pat in PVT_IP_REGEX]
+        if all(not r for r in regex_results):  # If all values were none (i.e. external IP)
+            try:
+                time.sleep(self.delay)
+                if ip not in self.ip_dict:  # Some other thread might've looked it up in the meantime (while we waited)
+                    ip_data = requests.get(API_ADDR.format(ip))
+                    self.num = self.num + 1
+                else:
+                    return self.ip_dict[ip]
+
+            except requests.exceptions.RequestException as e:  # All requests exceptions inherit from this exception
+                print(e)
+                return "Unknown"
+
+            data_dict = json.loads(ip_data.content)
+            if data_dict["status"] == "fail":
+                return "Private IP"  # Just incase it slipped through the regex.
+
+            else:
+                return data_dict["country"]
+        else:
+            return "Private IP"
--- client.py
+++ client.py
@@ -1,68 +1,18 @@
 from scapy3k.all import *
-import requests
 import json
 import subprocess
 import threading
+from CountryHandler import IpCountry
 
-API_ADDR = "http://ip-api.com/json/{}"
+
 PACKET_AMOUNT = 200
 LAN_DETAILS = {}
 PACKET_DATA = []
 MANAGER_DETAILS = ("127.0.0.1", 28972)
 NETSTAT_REGEX = r"({} .+)([^\[])* \[([a-zA-Z.]+)]"
-PVT_IP_REGEX = [re.compile(r"^127.\d{1,3}.\d{1,3}.\d{1,3}$"),
-                re.compile(r"^10.\d{1,3}.\d{1,3}.\d{1,3}$"),
-                re.compile(r"^192.168.\d{1,3}$"),
-                re.compile(r"^172.(1[6-9]|2[0-9]|3[0-1]).[0-9]{1,3}.[0-9]{1,3}$")]
+
 
 # Program must be ran in elevated privileges mode (i.e. Administrator) for the program field to work.
-
-
-class IpCountry:
-    def __init__(self):
-        self.ip_dict = {}  # Store associations
-        self.num = 0
-        self.delay = 1
-
-    def __getitem__(self, ip):
-        """
-        Gets the IP's country and caches it.
-        :param ip: The ip to get the country for.
-        :return: The country
-        """
-        if ip not in self.ip_dict:
-            self.ip_dict[ip] = self.get_country(ip)
-        return self.ip_dict[ip]
-
-    def get_country(self, ip: str) -> str:
-        """
-        Gets the country of an ip.
-        :param ip: String of an ip.
-        :return: Country string.
-        """
-
-        regex_results = [pat.match(ip) for pat in PVT_IP_REGEX]
-        if all(not r for r in regex_results):  # If all values were none (i.e. external IP)
-            try:
-                time.sleep(self.delay)
-                if ip not in self.ip_dict:  # Some other thread might've looked it up in the meantime (while we waited)
-                    ip_data = requests.get(API_ADDR.format(ip))
-                    self.num = self.num + 1
-                else:
-                    return self.ip_dict[ip]
-
-            except requests.exceptions.RequestException as e:  # All requests exceptions inherit from this exception
-                print(e)
-                return "Unknown"
-
-            data_dict = json.loads(ip_data.content)
-            if data_dict["status"] == "fail":
-                return "Private IP"  # Just incase it slipped through the regex.
-
-            else:
-                return data_dict["country"]
-        else:
-            return "Private IP"
 
 
 class PacketHandler(threading.Thread):
@@ -82,6 +32,7 @@ class PacketHandler(threading.Thread):
 
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
         sock.sendto(bytes(json.dumps(data_dict), "utf-8"), MANAGER_DETAILS)
+        print("Transmitted data package ({} packets)".format(PACKET_AMOUNT))
 
 
 def sniff_filter(packet: Packet):
@@ -92,7 +43,7 @@ def sniff_filter(packet: Packet):
 def get_netstat_output() -> str:
     """Returns the output of the netstat command."""
     netstat = subprocess.Popen(["netstat", "-nb"], stdout=subprocess.PIPE).stdout  # Use the netstat command
-    netstat = [r.decode() for r in netstat]  # Decode the bytes
+    netstat = [r.decode() for r in netstat]  # Decode the bytes for each line
     netstat = "".join(netstat)  # Join the output into a single string.
     return netstat
 
@@ -144,6 +95,7 @@ def record_details(packet: Packet):
 
 
 def get_local_details() -> dict:
+    """Gets the details about the local network."""
     p = srp1(Ether() / IP(dst="google.com", ttl=0) / ICMP() / "lorem", verbose=0)
     data_dict = {"gateway_ip": p[IP].src, "my_ip": p[IP].dst, "my_mac": p[Ether].dst}
     return data_dict
@@ -155,6 +107,7 @@ def adjust_delay(ip_class, thread_list):
         ip_class.delay = 0.5 * len(thread_list)  # Adjust the delay based on the number of threads running.
     else:
         ip_class.delay = 0.5
+
 
 def main():
     global LAN_DETAILS
@@ -172,7 +125,7 @@ def main():
             threads.append(curr_thread)
             adjust_delay(ip_country, threads)
             print("Added data package to queue ({} packets). {} awaiting transmission.".format(PACKET_AMOUNT
-                                                                                               , len(threads)))
+                                                                                               , len(threads) - 1))
 
     except KeyboardInterrupt:
         print("Aborted agent operation.")
