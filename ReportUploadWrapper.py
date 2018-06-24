10c2a75f16e49b2f734d0a0a87c1587eb466b001
Nir <nir@nirharel.space>
2018-06-24 21:14:24 +0300

    Add delay mechanism

--- ReportUploadWrapper.py
+++ ReportUploadWrapper.py
@@ -1,18 +1,22 @@
 import socket
+import time
 
 class ReportUploader:
     """A simple wrapper of the ReportUploader protocol."""
-    def __init__(self, html_path, username, server_details: tuple = ("54.71.128.194", 8808)):
+    def __init__(self, html_path, username, server_details: tuple = ("54.71.128.194", 8808), upload_frequency = 120):
         """
 
         :param html_path: The path to the HTML file we want to send to the server.
         :param username: The username to use the protocol with.
         :param server_details: A tuple containing the server address and port.
+        :param upload_frequency: The amount of seconds that must have passed from the last upload to upload again.
         """
         assert username  # Because we might get it from user input
         self.html_path = html_path
         self.username = username
         self.server_details = server_details
+        self.upload_frequency = upload_frequency
+        self.last_upload = 0
 
     def _read_file(self):
         """Internal use. Simply reads the html file the class was constructed with, and returns the string."""
@@ -21,25 +25,27 @@ class ReportUploader:
 
     def update_html(self):
         """Updates the html file on the server."""
-        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        conn.connect(self.server_details)
+        if (time.time() - self.upload_frequency) > self.last_upload:
+            self.last_upload = time.time()
+            conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            conn.connect(self.server_details)
 
-        # Ask the server if our username is valid
-        conn.send(f"400#USER={self.username}".encode())
+            # Ask the server if our username is valid
+            conn.send(f"400#USER={self.username}".encode())
 
-        if conn.recv(2048).decode() != "405#USER OK":
-            raise Exception("Invalid username!")
+            if conn.recv(2048).decode() != "405#USER OK":
+                raise Exception("Invalid username!")
 
-        html_contents = self._read_file()
-        conn.send(f"700#SIZE={len(html_contents)},HTML={html_contents}".encode())
+            html_contents = self._read_file()
+            conn.send(f"700#SIZE={len(html_contents)},HTML={html_contents}".encode())
 
-        resp = conn.recv(2048).decode()
-        if "705#FILE SAVED TO" not in resp:
-            print(resp)
-            raise Exception("Invalid HTML File!")
+            resp = conn.recv(2048).decode()
+            if "705#FILE SAVED TO" not in resp:
+                print(resp)
+                raise Exception("Invalid HTML File!")
 
-        conn.send("900#BYE".encode())  # Gracefully terminate.
-        conn.close()
+            conn.send("900#BYE".encode())  # Gracefully terminate.
+            conn.close()
 
 
 
